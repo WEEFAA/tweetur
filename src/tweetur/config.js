2f8da2bcc6ec31fa8741fea51ceb676e1c6df27a
WEEFAA <rayranilfu7o7@gmail.com>
2020-05-03 20:29:46 +0800

    --added config for Tweetur User Credentials

---
+++ src/tweetur/config.js
@@ -0,0 +1,8 @@
+
+// parameters for instancing Tweetur Class
+exports.TWEETUR_CREDENTIALS = {
+	consumer_key: "string",
+	consumer_secret: "string",
+	access_token: "string",
+	access_token_secret: "string"
+}
