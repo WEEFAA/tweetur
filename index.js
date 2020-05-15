4e05abdae040913d6dfe83f6b977a40f5b000f36
WEEFAA <rayranilfu7o7@gmail.com>
2020-05-15 18:05:04 +0800

    --clean index.js

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 const Tweetur = require('./src/tweetur')
-
-const keys = {
-	consumer_key: "mPASZqPi3m0HLcA6pdPefjSki",
-	consumer_secret: "rqgvc4JtC58DM9yQkZfg8VEfydWeg2U1r5fKB4wmitLmCWhGGh",
-	access_token: "2442827221-3PKQEjxyFtE3JymHOfUYjtpDDrrCrGjDQo0HVud",
-	access_token_secret: "9PenTj0NvndEAdsxdiN1mJ6BObKGPmcOu2yrKo7V3sTDq"
-}
-
-const me = new Tweetur(keys)
-console.log(me)
-;(async function(){
-	try{
-		await me.authenticate()
-		const result = await me.revoke() 
-		console.log(result)
-	}catch(e){
-		console.error(e)
-	}
-})()
+const { 
+	SIGN_METHOD,
+	OAUTH_VERSION,
+	TWEETUR_CREDENTIALS,
+	TWEETUR_KEYS_SCHEMA
+} = require('./src/tweetur/config')
 
 // export tweetur module
 module.exports = Tweetur
+exports.SIGN_METHOD = SIGN_METHOD
+exports.OAUTH_VERSION = OAUTH_VERSION
+exports.TWEETUR_CREDENTIALS = TWEETUR_CREDENTIALS
+exports.TWEETUR_KEYS_SCHEMA = TWEETUR_KEYS_SCHEMA
