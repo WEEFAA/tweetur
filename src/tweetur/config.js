c78cfc541a9eb6098bf6a2cb0237325b147791c2
WEEFAA <rayranilfu7o7@gmail.com>
2020-05-15 12:26:54 +0800

    --adding credentials schema

--- src/tweetur/config.js
+++ src/tweetur/config.js
@@ -7,3 +7,35 @@ exports.TWEETUR_CREDENTIALS = {
 	access_token: "string",
 	access_token_secret: "string"
 }
+
+// Tweetur credentials and keys
+exports.TWEETUR_KEYS_SCHEMA = {
+	consumer_key: {
+		type: "string",
+		required: true
+	},
+	consumer_secret: {
+		type: "string",
+		required: true
+	},
+	access_token: {
+		type: "string",
+		required: true
+	},
+	access_token_secret: {
+		type: "string",
+		required: true
+	},
+	sub: {
+		type: "string",
+		required: false,
+		default: 'api', // default twitter sub domain for accessing API
+		regexp: new RegExp(/^\w+-*\w*$/, 'i')
+	},
+	api_version: {
+		type: "string",
+		required: false,
+		default: '1.1', // default twitter api version
+		regexp: new RegExp(/^\d+\.{0,1}\d*(\d+\.{0,1})*$/)
+	}
+}
